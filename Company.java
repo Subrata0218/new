afe73d8de422a8cf22b47464e1ef1c487a9c5e4f
Subrata0218 <Subratarishi500@gmail.com>
2021-11-26 17:02:27 +0530

    My First Commit

---
+++ Company.java
@@ -0,0 +1,6 @@
+public class Company{
+                   public static void main(String [] args){
+                   System.out.println("Name Of The Company : LTI");
+                   System.out.println("Name Of The Employee : Subrata Rishi");
+                  }
+}
---
+++ Profile.java
@@ -0,0 +1,6 @@
+public class Profile2{
+            public static void main(String[ ]  args){
+             System.out.println("Rishi Subrata");
+             System.out.println("KOLKATA");
+           }
+}
